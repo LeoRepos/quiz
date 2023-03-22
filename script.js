3d9cf8fb392ea56e9cd95cf13cb0972b970b4ef6
Leonardo Dos Santos Dias <dleonardo@adhosting.internal>
2023-03-22 16:12:02 -0300

    feat/ Conclusão do projeto

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
 let currentQuestion = 0;
 let correctAnswer   = 0;
 
-
 showQuestion();
+
+document.querySelector('#button').addEventListener('click', tryAgain);
 
 function showQuestion(){
     if (questions[currentQuestion]) {
-        let q   = questions[currentQuestion];    
+        let q   = questions[currentQuestion];
         let pct = Math.floor((currentQuestion / questions.length) * 100);
 
-        document.querySelector('.progress--bar').style.width = `${pct}%`;
-
-        document.querySelector('.scoreArea').style.display = 'none';
+        document.querySelector('.progress--bar').style.width  = `${pct}%`;
+        document.querySelector('.scoreArea').style.display    = 'none';
         document.querySelector('.questionArea').style.display = 'block';
-        document.querySelector('.question').innerHTML = q.question;
-        document.querySelector('.options').innerHTML = '';
+        document.querySelector('.question').innerHTML         = q.question;
+        document.querySelector('.options').innerHTML          = '';
 
         let optionHtml = '';
         for (let i in q.options) {
             optionHtml += `<div data-op="${i}" class="option"><span>${parseInt(i)+1}</span>${q.options[i]}</div>`;
-        }
+        };
         document.querySelector('.options').innerHTML = optionHtml;
-
         document.querySelectorAll('.options .option').forEach(item => {
             item.addEventListener('click', optionClickEvent);
         });
 
     } else {
         finishQuiz();
-
-    }
+    };
 };
 
 function optionClickEvent(e) {
@@ -46,14 +44,24 @@ function optionClickEvent(e) {
 function finishQuiz() {
 
     let points = Math.floor((correctAnswer / questions.length) * 100);
-    document.querySelector('.scorePct').innerHTML = `Acertou ${points}%`;
-    // document.querySelector('.scoreText2').innerHTML = `Você respondeu ${questions} questões e acertou ${correctAnswer}.`
-    
-    document.querySelector('.scoreArea').style.display = 'block';
-    document.querySelector('.questionArea').style.display = 'none';
-    document.querySelector('.progress--bar').style.width = '100%';    
+    if(points < 30){
+        document.querySelector('.scoreText1').innerHTML     = `Tá ruim em?!`; 
+        document.querySelector('.scorePct').style.color     = '#F00';
+    } else if (points >= 30 && points < 50) {
+        document.querySelector('.scoreText1').innerHTML     = `Muito bom, mas pode melhorar!`; 
+        document.querySelector('.scorePct').style.color     = 'yellow';
+    };
+
+    document.querySelector('.scorePct').innerHTML           = `Acertou ${points}%`;
+    document.querySelector('.scoreText2').innerHTML         = `Você respondeu ${questions.length} questões e acertou ${correctAnswer}.`    
+    document.querySelector('.scoreArea').style.display      = 'block';
+    document.querySelector('.questionArea').style.display   = 'none';
+    document.querySelector('.progress--bar').style.width    = '100%';
 };
 
-
-
+function tryAgain() {
+    correctAnswer   = 0;
+    currentQuestion = 0;
+    showQuestion();
+};
 
